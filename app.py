8164447bea94bd26b444ee6c9a397b995cbdbd9e
arkhangelx <asaa3730@gmail.com>
2026-03-31 17:53:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
+st.set_page_config(page_title="Test", layout="wide")
+
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive",
@@ -9,20 +11,29 @@ SCOPES = [
 
 SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1kuJ609rOK-G8mn1mG935W8QswqbsHov7o02GDcCss48/edit?usp=sharing"
 
-st.write("Старт приложения")
+st.title("Проверка запуска")
+
+st.write("1. Приложение стартовало")
 
 try:
+    creds_dict = dict(st.secrets["google"])
+    st.write("2. Secrets прочитаны")
+
     credentials = Credentials.from_service_account_info(
-        dict(st.secrets["google"]),
+        creds_dict,
         scopes=SCOPES,
     )
-    st.write("Secrets прочитаны")
+    st.write("3. Credentials созданы")
 
     gc = gspread.authorize(credentials)
-    st.write("gspread авторизован")
+    st.write("4. Авторизация gspread прошла")
 
     sh = gc.open_by_url(SPREADSHEET_URL)
-    st.write("Таблица открыта:", sh.title)
+    st.write("5. Таблица открыта")
+
+    worksheets = sh.worksheets()
+    st.write("6. Листы получены")
+    st.success([ws.title for ws in worksheets])
 
 except Exception as e:
-    st.error(str(e))
+    st.error(f"Ошибка: {e}")
